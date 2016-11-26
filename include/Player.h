1fe73ec0897e336a4d6bd5e3de9cd9c5f18bdd20
liorgre <liorgre@post.bgu.ac.il>
2016-11-26 11:10:19 +0200

    Lior changes

--- include/Player.h
+++ include/Player.h
@@ -9,26 +9,29 @@ using namespace std;
 
 class Player : public Hand {
 private:
-	const string name;
+	std::string name; //did amit do it or did they and can i change it??
+	int location;
 public:
+	Player::Player(std::string * n, int loc);
 	string getName();   //Returns the name of the player
-	virtual CardRequest playTurn();
 };
 
 class PlayerType1 : public Player {  //For strategy 1
-//...
+    virtual TakeFromHeighestCardRequest playTurn();
 };
 
 class PlayerType2 : public Player {  //For strategy 2
-//...
+    virtual TakeFromHeighestCardRequest playTurn();
 };
 
 class PlayerType3 : public Player {  //For strategy 3
-//...
+    virtual InOrderCardRequest playTurn();
+    int lastRequest(); //returns the id of the last player from whom this player asked for a card
 };
 
 class PlayerType4 : public Player {  //For strategy 4
-//...
+    virtual InOrderCardRequest playTurn();
+    int lastRequest(); //returns the id of the last player from whom this player asked for a card
 };
 
 #endif
