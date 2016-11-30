e13dda2362a019a9c88b3b6385f04116344b92a7
Amit Marx <amitmarx@gmail.com>
2016-11-30 16:58:53 +0200

    Binary search

--- include/Hand.h
+++ include/Hand.h
@@ -9,20 +9,21 @@ using namespace std;
 
 class Hand {
 protected:
-	vector<Card*> getInHandCards();
+    vector<Card*> getInHandCards();
 private:
-	// Declare here the collection of "Card *" of the hand
-	vector<Card*> inHand;
+    // Declare here the collection of "Card *" of the hand
+    vector<Card*> inHand;
 public:
-	Hand();
-	Hand(const Hand &otherHand);
-	virtual ~Hand();
-	bool addCard(Card &card);
-	bool removeCard(Card &card);
-	vector<Card*> tryGetCards(string value);
-	int getNumberOfCards(); // Get the number of cards in hand
-	string toString(); // Return a list of the cards, separated by space, in one line,in a sorted order, ex: "2S 5D 10H"
-	void remove4ofAKind();
+    Hand();
+    Hand(const Hand &otherHand);
+    virtual ~Hand();
+    bool addCard(Card &card);
+    bool removeCard(Card &card);
+    vector<Card*> tryGetCards(string value);
+    int getNumberOfCards(); // Get the number of cards in hand
+    string toString(); // Return a list of the cards, separated by space, in one line,in a sorted order, ex: "2S 5D 10H"
+    void remove4ofAKind(string s);
+    int binarySearch(std::vector<Card *> vec, int start, int end, Card &card);
 };
 
 #endif
--- src/Hand.cpp
+++ src/Hand.cpp
@@ -49,17 +49,20 @@ bool Hand::addCard(Card &card){
         }
     }
     inHand.insert(inHand.begin()+i, &card);
-    remove4ofAKind();
+    string s=card.toString().substr(0,(card.toString()).length()-1);
+    remove4ofAKind(s);
     return true;
 };
 
 bool Hand::removeCard(Card &card){
-    for(int i=0;i<inHand.size();i++){
-        if(card.toString()==inHand[i]->toString()){
-            inHand.erase(inHand.begin()+i);
-            break;
-        }
-    }
+    int x= binarySearch(getInHandCards(),0,getInHandCards().size()-1,card);
+    inHand.erase(inHand.begin()+x);
+//    for(int i=0;i<inHand.size();i++){
+//        if(card.toString()==inHand[i]->toString()){
+//            inHand.erase(inHand.begin()+i);
+//            break;
+//        }
+//    }
     return true;
 };
 
@@ -75,25 +78,39 @@ string Hand::toString() {
     return returnStr.substr(0,returnStr.size()-1);
 }
 
-void Hand::remove4ofAKind() {
+void Hand::remove4ofAKind(string s) {
     int counter=1;
-
     for(int i=1;i<inHand.size();i++) {
-        if (inHand[i]->isValueEqual(*inHand[i - 1])) {
-            counter++;
-        }
-        else {
-            counter = 1;
-        }
-        if (counter == 4) {
-            for (int x = i - 3; x <= i; x++) {
-                delete (inHand[x]);
+        if (inHand[i]->toString().substr(0,inHand[i]->toString().length()-1)==s){
+            if (inHand[i]->isValueEqual(*inHand[i - 1])) {
+                counter++;
             }
-            inHand.erase(inHand.begin() + (i - 3), inHand.begin() + i+1);
-            i -= 3;
-            counter = 1;
+            else {
+                counter = 1;
+            }
+            if (counter == 4) {
+                for (int x = i - 3; x <= i; x++) {
+                    delete (inHand[x]);
+                }
+                inHand.erase(inHand.begin() + (i - 3), inHand.begin() + i+1);
+                i -= 3;
+                counter = 1;
+            }
         }
+    }}
+
+int Hand::binarySearch(std::vector<Card *> vec, int start, int end, Card &card) {
+    if(start > end) {
+        return -1;
     }
-}
+    const int middle = start + ((end - start) / 2);
+    if(vec[middle]->isValueEqual(card)){
+        return middle;
+    }
+    else if(vec[middle]->compareTo(card)==1){
+        return binarySearch(vec, start, middle - 1, card);
+    }
+    return binarySearch(vec, middle + 1, end, card);
+};
 
 
